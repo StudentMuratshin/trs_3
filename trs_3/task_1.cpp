2884004a9223ddcb9f63d4f276b099d3280f2f14
ilgam_muratshin <muratshin.ilgam96@gmail.com>
2023-04-12 13:11:10 +0500

    asdf

---
+++ trs_3/task_1.cpp
@@ -0,0 +1,19 @@
+﻿#include "trs_3.h"
+
+using namespace std;
+
+int main()
+{
+	const int n = 200;
+	const int m = 400;
+	const int lx = 2, ly = 1;
+	double hx = static_cast<double>(lx) / n;
+	double hy = static_cast<double>(ly) / m;
+	vector < vector <double> > u(n, vector <double>(m));
+	vector < vector <double> > f(n, vector <double>(m));
+
+	for (int i = 1; i < m; i++)
+	{
+		u[0][i] = 0;
+	}
+}
---
+++ trs_3/trs_3.h
@@ -0,0 +1,9 @@
+﻿// trs_3.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+// TODO: Reference additional headers your program requires here.
